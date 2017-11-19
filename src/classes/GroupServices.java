980622366cd63114e7274f2ccae675b46d6b47aa
58011150Wichayut <31825106+wichayut@users.noreply.github.com>
2017-11-19 17:30:10 +0700

    create GroupServices class
    by Wichayut

---
+++ src/classes/GroupServices.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.*;
+import smartreminder.*;
+
+/**
+ *
+ * @author kan
+ */
+public class GroupServices {
+    //create an object of PersonalCalendar
+    private static GroupServices instance = new GroupServices();
+    
+    private List<GroupCatalogue> groupList = new ArrayList<>();
+    
+    private GroupServices(){
+    
+        EntityManager em = SmartReminder.emf.createEntityManager();
+ 
+        // Store 1000 Point objects in the database:
+        em.getTransaction().begin();
+        
+        TypedQuery<GroupCatalogue> query = em.createQuery("SELECT gCat FROM GroupCatalogue gCat", GroupCatalogue.class);
+        groupList = query.getResultList();
+        
+        em.close();
+        
+    }
+    
+    public static GroupServices getInstance(){
+        return instance;
+    }
+    
+    public void create(GroupCatalogue groupCat) {
+        
+        boolean isValid = true;
+        
+        for (int i = 0; i < groupList.size(); i++) {
+            if (groupList.get(i).getGroupDetail().getGroupName().equals(groupCat.getGroupDetail().getGroupName())) {
+                isValid = false;
+                break;
+            }
+        }
+        if (isValid) {
+            EntityManager em = SmartReminder.emf.createEntityManager();
+            em.getTransaction().begin();
+            em.persist(groupCat);
+            em.getTransaction().commit();
+            // Close the database connection:
+            em.close();
+            groupList.add(groupCat);
+            System.out.println("Creating group success!!");
+        }
+        else {
+            System.out.println("This group's name is used");
+        }
+        
+    }
+    
+    public void delete(GroupCatalogue groupCat) {
+        groupList.remove(groupCat);
+    }
+    
+    public ArrayList getMyGroupList() {
+        ArrayList<GroupCatalogue> list = new ArrayList<>();
+        for (int i = 0; i < groupList.size(); i++) {
+            if (groupList.get(i).getUserAccount().getId() == SmartReminder.myAccount.getId()) {
+                list.add(groupList.get(i));
+            }
+        }
+        return list;
+    }
+    
+}
